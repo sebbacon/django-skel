8b05472c1e59b8189b579d42bf1cf898657182eb
Seb Bacon <seb.bacon@gmail.com>
2010-07-14 10:56:33 +0100

    fix merge and make sure we only apply to text/html

--- analytics_middleware.py
+++ analytics_middleware.py
@@ -12,21 +12,13 @@ ga_html = """
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
-<<<<<<< HEAD:analytics_middleware.py
 </script></head>"""
-=======
-</script>"""
->>>>>>> fa5c330d807f6343e74f169a739815b9f4467c22:analytics_middleware.py
 
 class GoogleAnalyticsMiddleware:
     def process_response(self, request, response):
         ga_id = settings.GOOGLE_ANALYTICS_ID
-        if ga_id:
+        if ga_id and "text/html" in response.get('content-type', ''):
             current = response.content
             replacement = ga_html % ga_id
-<<<<<<< HEAD:analytics_middleware.py
             response.content = current.replace("</head>", replacement)
-=======
-            response.content = current.replace("</body>", replacement)
->>>>>>> fa5c330d807f6343e74f169a739815b9f4467c22:analytics_middleware.py
         return response
